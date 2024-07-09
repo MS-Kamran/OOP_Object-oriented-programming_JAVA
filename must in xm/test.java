fc3b64514ef830213009a702d2a2e06b0597b3ab
MS-Kamran <100725506+MS-Kamran@users.noreply.github.com>
2024-07-09 16:24:04 +0600

    Add files via upload

---
+++ must in xm/test.java
@@ -0,0 +1,13 @@
+public class test {
+    public static void main (String [] args){
+        Gandalf var1 = new Frodo();
+        Gandalf var2 = new Bilbo();
+        Gandalf var3 = new Gandalf();
+        Object var4 = new Bilbo();
+        Bilbo var5 = new Frodo();
+        Object var6 = new Gollum();
+                      
+
+        
+    }
+}
